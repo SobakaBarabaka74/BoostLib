962bb2654ee09c12a64d707cc290ec64ebd64b26
SobakaBarabaka74 <153353273+SobakaBarabaka74@users.noreply.github.com>
2024-03-05 01:25:45 +0700

    Add files via upload

--- client/room/BoostLib.js
+++ client/room/BoostLib.js
@@ -1,1 +1,32 @@
+import {
+    Teams, Players, Properties
+} as Api from 'pixel_combats/room'
 
+
+export const Boost = {
+    
+    SaveList: {
+        Enable: false, 
+        List: []
+    }, 
+    
+};
+
+
+Api.Teams.OnRequestJoinTeam.Add((p) => {
+    if (Boost.SaveList.Enable) my_Load(p)
+})
+
+Api.Players.OnPlayerDisconnected.Add((p) => {
+    if (Boost.SaveList.Enable) my_Save(p)
+})
+
+function my_Load(p) {
+    for (let prop of Boost.SaveList.List)
+        p.Properties.Get(prop.Name).Value = Api.Properties.GetContext().Get(prop.Name + p.Id)
+}
+
+function my_Save(p) {
+    for (let prop of Boost.SaveList.List)
+        Api.Properties.GetContext().Get(prop.Name + p.Id).Value = p.Properties.Get(prop.Name).Value
+}
