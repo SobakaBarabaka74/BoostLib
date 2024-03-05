c0e3030894330f00f5a4967598d25664e9268932
SobakaBarabaka74 <153353273+SobakaBarabaka74@users.noreply.github.com>
2024-03-05 19:40:13 +0700

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -5,12 +5,12 @@ import {
 import { Color } as basic from 'pixel_combats/basic'
 
 boost.Boost.SaveList.Enable = true
-boost.Boost.SaveList.List = [{
+boost.Boost.SaveList.List = [
   {
     Name: "Kills", 
     Default: 0
   }
-}]
+]
 
 api.Teams.Add("Blud", "Teams/Blue", new basic.Color(0, 0, 1, 0)) 
 
